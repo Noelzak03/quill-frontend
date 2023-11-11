bb3906b1cc113a0aeff99ca44ababfdaff53d654
Noel Binoy <noelzakbin03@gmail.com>
2023-11-11 19:23:41 +0530

    Text and initial button configuration added

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,28 @@
 "use client";
 import React from "react";
 import Particle from "../components/Particle";
+import Quill from "../components/Quill";
 function page() {
   return (
-    <>
-      <div className="z-0 absolute">
+    <div>
+      <div className="z-0 ">
         <Particle />
       </div>
-      <div className="z-10 absolute bg-transparent flex h-full w-full items-center justify-center">
-        <h1 className="text-8xl text-white text-center">Quill</h1>
+      <div className="z-10 absolute  bg-transparent p-10 ">
+        <Quill />
       </div>
-    </>
+      <div className="z-10 relative h-screen w-full flex-col flex justify-center items-center bg-transparent">
+        <p className="text-center text-white mb-36 mx-6 text-6xl font-lexend font-medium">
+          Join Quill, and embark on your creative voyage.
+        </p>
+        <div className="z-10 relative flex  gap-20 justify-center  items-center  ">
+          <button className="bg-white p-3 border border-{'#715aff'} border-4 font-lexend font-">
+            Create Room
+          </button>
+          <button className="bg-white p-3  font-lexend">Join Room</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Quill.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Quill() {
+  return (
+    <Link
+      href="/"
+      className="p-0 sm:pl-6 font-bold text-transparent font-lexend text-6xl bg-gradient-to-r from-purple-400 to-pink-600  bg-clip-text "
+    >
+      Quill
+    </Link>
+  );
+}
