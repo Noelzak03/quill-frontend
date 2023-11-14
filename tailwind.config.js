80547ab8ead34713e1259bf858b96c59920858db
Noel Binoy <noelzakbin03@gmail.com>
2023-11-14 09:47:07 +0530

    Resolved errors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,7 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
-      colors: 
-        primary: '#715AFF',
-        secondary: '#000000',
+
       colors: {
         primary: "#715AFF",
         secondary: "#000000"
