39cff00c978e6d3c93b1fb16f5e174715f34dab7
Noel Binoy <noelzakbin03@gmail.com>
2023-11-21 16:46:07 +0530

    Updating Chat component UI for text message

--- src/app/components/Chatbox.jsx
+++ src/app/components/Chatbox.jsx
@@ -10,22 +10,30 @@ export default function Chat({ chatMessages, sendJsonMessage }) {
     setFormValue("");
   };
   const chatElement = chatMessages.map((msg) => (
-    <p>{`${msg.username}: ${msg.message}`}</p>
+    <div className="flex flex-col  gap-1 mb-2">
+      <p className=" text-base items-start justify-start text-white">
+        {msg.username}
+      </p>
+      <p className="text-3xl text-white">{msg.message}</p>
+    </div>
+    // <p>{`${msg.username}: ${msg.message}`}</p>
   ));
   return (
-    <div className="h-[42rem] w-[28rem] border-primary  border-4 bg- items-self-center flex flex-col justify-evenly">
-      <div className=" text-left text-black  h-1/6 w-full p-8 text-5xl  bg-primary font-lexend font-bold">
-        <p>Chat</p>
+    <div className="h-[42rem] w-[28rem]   border-4 border-primary items-self-center flex flex-col justify-evenly">
+      <div className=" flex text-left  text-primary h-1/6 w-full  text-5xl  bg-black border-b-4 border-primary font-lexend font-semibold justify-start items-center">
+        <p className="p-0 sm:pl-6 font-lexend font-bold text-transparent text-6xl bg-gradient-to-r from-purple-400 to-pink-600  bg-clip-text ">
+          Chat
+        </p>
       </div>
-      <div className="h-2/3 w-full bg-slate-500 ">
-        <div className="text-left text-black  h-1/6 w-full p-8 text-5xl  bg-primary font-lexend font-bold">
+      <div className="h-2/3 w-full bg-black">
+        <div className="text-left text-black  h-1/6 w-full p-8 text-5xl   font-lexend font-bold">
           {chatElement}
         </div>
       </div>
       <form className="w-full h-1/6 px-2" onSubmit={sendMessage}>
         <div className="flex items-stretch py-4 m-2 mt-4 border-4 border-primary ">
           <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             value={formValue}
             onChange={(e) => setFormValue(e.target.value)}
